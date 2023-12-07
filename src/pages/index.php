05e1d721c76cf4ce170c14107d5d6287bec55f1d
youkwhd <lolywk@tutanota.com>
2023-12-07 22:50:09 +0700

    chore(css): add spaces

--- src/pages/index.php
+++ src/pages/index.php
@@ -35,60 +35,64 @@
     <title>Hacker N3ws</title>
 </head>
 <body>
-    <?= render_header() ?>
-    <?php for ($i = $from; $i < $top_stories_len && $i < $to; $i++) : ?>
-        <?php
-            $story = get_from_inet_or_cache($curl, $memcached, "news-$top_stories[$i]", "https://hacker-news.firebaseio.com/v0/item/$top_stories[$i].json");
-        ?>
-        <div class="hn--container hn--news-container">
-            <div>
-                <span class="hn--news-title">
-                    <a target="_blank" href="<?= $story->{"url"} ?>">
-                        <?= $story->{"title"} ?>
-                    </a>
-                </span>
-                <br />
-                <small class="hn--news-host-container">
-                <?= $story->{"by"} ?> — (<a class="hn--news-host" target="_blank" href="<?= sprintf("%s://%s", parse_url($story->{"url"}, PHP_URL_SCHEME), parse_url($story->{"url"}, PHP_URL_HOST)) ?>"><?= parse_url($story->{"url"}, PHP_URL_HOST) ?></a>)
-                </small>
-                <div class="hn--news-bottom">
-                    <div>
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="100%" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M277.375 427V167.296l119.702 119.702L427 256 256 85 85 256l29.924 29.922 119.701-118.626V427h42.75z"></path></svg>
-                        <span>
-                            <?= $story->{"score"} ?>
-                        </span>
-                    </div>
-                    <div>
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="100%" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M144 208c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm112 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm112 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zM256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 368c-26.7 0-53.1-4.1-78.4-12.1l-22.7-7.2-19.5 13.8c-14.3 10.1-33.9 21.4-57.5 29 7.3-12.1 14.4-25.7 19.9-40.2l10.6-28.1-20.6-21.8C69.7 314.1 48 282.2 48 240c0-88.2 93.3-160 208-160s208 71.8 208 160-93.3 160-208 160z"></path></svg>
-                        <span>
-                            <?= $story->{"descendants"} ?>
-                        </span>
+    <main>
+        <?= render_header() ?>
+        <?php for ($i = $from; $i < $top_stories_len && $i < $to; $i++) : ?>
+            <?php
+                $story = get_from_inet_or_cache($curl, $memcached, "news-$top_stories[$i]", "https://hacker-news.firebaseio.com/v0/item/$top_stories[$i].json");
+            ?>
+            <div class="hn--container hn--news-container">
+                <div>
+                    <span class="hn--news-title">
+                        <a target="_blank" href="<?= $story->{"url"} ?>">
+                            <?= $story->{"title"} ?>
+                        </a>
+                    </span>
+                    <br />
+                    <small class="hn--news-host-container">
+                    <?= $story->{"by"} ?> — (<a class="hn--news-host" target="_blank" href="<?= sprintf("%s://%s", parse_url($story->{"url"}, PHP_URL_SCHEME), parse_url($story->{"url"}, PHP_URL_HOST)) ?>"><?= parse_url($story->{"url"}, PHP_URL_HOST) ?></a>)
+                    </small>
+                </div>
+                <div>
+                    <div class="hn--news-bottom">
+                        <div>
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="100%" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M277.375 427V167.296l119.702 119.702L427 256 256 85 85 256l29.924 29.922 119.701-118.626V427h42.75z"></path></svg>
+                            <span>
+                                <?= $story->{"score"} ?>
+                            </span>
+                        </div>
+                        <div>
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="100%" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M144 208c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm112 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zm112 0c-17.7 0-32 14.3-32 32s14.3 32 32 32 32-14.3 32-32-14.3-32-32-32zM256 32C114.6 32 0 125.1 0 240c0 47.6 19.9 91.2 52.9 126.3C38 405.7 7 439.1 6.5 439.5c-6.6 7-8.4 17.2-4.6 26S14.4 480 24 480c61.5 0 110-25.7 139.1-46.3C192 442.8 223.2 448 256 448c141.4 0 256-93.1 256-208S397.4 32 256 32zm0 368c-26.7 0-53.1-4.1-78.4-12.1l-22.7-7.2-19.5 13.8c-14.3 10.1-33.9 21.4-57.5 29 7.3-12.1 14.4-25.7 19.9-40.2l10.6-28.1-20.6-21.8C69.7 314.1 48 282.2 48 240c0-88.2 93.3-160 208-160s208 71.8 208 160-93.3 160-208 160z"></path></svg>
+                            <span>
+                                <?= $story->{"descendants"} ?>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <?php endfor ?>
-    <footer class="hn--footer-root">
-        <?php
-            $prev = $page - 1;
-            $next = $page + 1;
+        <?php endfor ?>
+        <footer class="hn--footer-root">
+            <?php
+                $prev = $page - 1;
+                $next = $page + 1;
 
-            $max_page = (int)($top_stories_len / $_CONFIG["NEWS_PER_PAGE"]);
+                $max_page = (int)($top_stories_len / $_CONFIG["NEWS_PER_PAGE"]);
 
-            if ($prev <= 0 || $prev > $max_page) {
-                $prev = $max_page;
-            }
+                if ($prev <= 0 || $prev > $max_page) {
+                    $prev = $max_page;
+                }
 
-            if ($next > $max_page) {
-                $next = 1;
-            }
-        ?>
-        <div class="hn--footer-left">
-            <a href="/?p=<?= $prev ?>">←</a>
-        </div>
-        <div class="hn--footer-right">
-            <a href="/?p=<?= $next ?>">→</a>
-        </div>
-    </footer>
+                if ($next > $max_page) {
+                    $next = 1;
+                }
+            ?>
+            <div class="hn--footer-left">
+                <a href="/?p=<?= $prev ?>">←</a>
+            </div>
+            <div class="hn--footer-right">
+                <a href="/?p=<?= $next ?>">→</a>
+            </div>
+        </footer>
+    <main>
 </body>
 </html>
