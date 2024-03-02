cbd7c5a5fb6df27e98d17bb9933a5f5a732cb460
youkwhd <lolywk@tutanota.com>
2024-03-02 12:47:35 +0700

    chore: check for SSL errors (?)

--- src/cache.php
+++ src/cache.php
@@ -7,6 +7,16 @@ function get_from_inet_or_cache($curl, $memcached, $cache_key, $url_endpoint) {
     curl_setopt($curl, CURLOPT_URL, $url_endpoint);
     $obj = json_decode(curl_exec($curl)) ?: [];
 
+    // Invalid SSL error
+    if (curl_errno($curl) == 60) {
+        echo curl_error($curl);
+        echo "<br/>";
+        echo "<br/>";
+        echo "You can ignore SSL verification by specifying:";
+        echo "<br/>";
+        echo "<code>curl_setopt(\$curl, CURLOPT_SSL_VERIFYPEER, false);</code>";
+    }
+
     // cache for 5 minutes
     if ($memcached)
         $memcached->set($cache_key, $obj, time() + 60 * 5);
